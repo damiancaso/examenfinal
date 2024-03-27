bb420f493d6237fce877d200eedcbc407b402181
damiancaso <125307407+damiancaso@users.noreply.github.com>
2024-03-27 10:51:10 -0500

    exmenn

--- examen.py
+++ examen.py
@@ -1,16 +1,7 @@
 from flask import Flask
 app=Flask(__name__)
 
-user_data = {
-    'name': 'Damian Caso',
-	'carrera':'desarrollo de sistemas',
-    'photo': 'https://i.pinimg.com/736x/fc/61/bb/fc61bb9edcd2fbd6867a885e62141a24.jpg',
-    'social_media': {
-        'Twitter': 'https://twitter.com/jose',
-        'LinkedIn': 'https://linkedin.com/in/jose',
-        # Add more social media links as needed
-    }
-}
+
 
 @app.route('/')
 def hello_geek():
@@ -26,11 +17,11 @@ def hello_geek():
     <div>
         <h2> Damian Caso </h2>
         <img src="https://i.pinimg.com/736x/fc/61/bb/fc61bb9edcd2fbd6867a885e62141a24.jpg" alt="User Photo">
-		<h1>{{user.carrera}}</h1>
+		<h1>Desarrollo de sistemas de la información </h1>
         <p>Social Media:</p>
         <ul>
             {% for platform, link in user.social_media.items() %}
-                <li>{{ platform }}: <a href="https://twitter.com/jose">https://linkedin.com/in/jose'</a></li>
+                <li>{{ platform }}: <a href="https://twitter.com/samuel">https://linkedin.com/in/samuel'</a></li>
             {% endfor %}
         </ul>
     </div>
